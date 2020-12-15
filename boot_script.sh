88dbf3a6bf18aa0922c5fbc889e04a517d77e15e
Nguyen Quoc Anh <beasthunter97@gmail.com>
2020-12-15 15:52:57 +0700

    test

--- boot_script.sh
+++ boot_script.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
-
 source /home/mendel/.bashrc
 cd /home/mendel/coral/DFM_FR/
 mkdir -p temp/ log/
 sudo mount -t ntfs-3g /dev/mmcblk1p1 ./temp/
 
 (
+    while true
+    do
+        count=`ping -c 1 8.8.8.8 | grep bytes | wc -l`
+        if [ $count -gt 1 ]
+        then
+            break
+        fi
+    done
+    commit=$(date)
+    git pull > log/update_log.txt
+    git add .
+    git commit -m "$commit"
+    git push
+    count=`cat log/update_log.txt | grep "Already up to date" | wc -l`
+    if [ $count -eq 0 ]
+    then
+        sudo reboot
+    fi
+
     echo "false" > log/working
     sleep 60s
     working=$(cat log/working)
     if ! $working
     then
         sudo reboot
-    else
-        while true
-        do
-            count=`ping -c 1 8.8.8.8 | grep bytes | wc -l`
-            if [ $count -gt 1 ]
-            then
-                break
-            fi
-        done
-        commit=$(date)
-        git pull > log/update_log.txt
-        git add .
-        git commit -m "$commit"
-        git push
-        count=`cat log/update_log.txt | grep "Already up to date" | wc -l`
-        if [ $count -eq 0 ]
-        then
-            sudo reboot
-        fi
     fi
 ) &
 
